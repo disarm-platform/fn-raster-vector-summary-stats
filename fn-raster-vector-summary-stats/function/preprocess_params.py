d83de7af4f127fdb33c8ce8c215743b06b1e3c70
Jonathan Smith <jonathan@peoplesized.com>
2019-02-27 11:22:04 +0200

    simpler params checking

--- fn-raster-vector-summary-stats/function/preprocess_params.py
+++ fn-raster-vector-summary-stats/function/preprocess_params.py
@@ -1,4 +1,4 @@
-from preprocess_helpers import write_temp_from_url_or_base64, required_boolean, required_exists
+from preprocess_helpers import write_temp_from_url_or_base64, required_exists
 
 
 def preprocess(params: dict):
@@ -8,13 +8,15 @@ def preprocess(params: dict):
     required_exists('raster', params)
     write_temp_from_url_or_base64('raster', params)
 
-    required_boolean('geojson_out', params)
+    if 'geojson_out' in params:
+        if type(params['geojson_out']) is not bool:
+            raise ValueError('`geojson_out` needs to be a boolean')
 
-    # params['stats'] if exists, must only include 'max',
+    # params['stats'] if exists must match the possible options
     if 'stats' in params:
         stats = params['stats']
-        all_possible_options = ['max', 'min', 'mean']
+        all_possible_options = ['min', 'max', 'mean', 'count', 'sum', 'std', 'median', 'majority', 'minority', 'unique', 'range', 'nodata']
 
         for stat in stats.split(' '):
             if stat not in all_possible_options:
-                raise ValueError(f'Unsupported `stat` parameter of \'{stat}\'')
+                raise ValueError(f'Unsupported `stat` parameter of "{stat}"')
